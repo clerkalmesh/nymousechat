9544f216929b174f1eaa293ba5d120874a1c11d9
memesh <clerkalmesh@gmail.com>
2026-02-15 15:03:32 +0700

    my update

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,52 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import useAuthStore from "./store/useAuthStore";
-import { useThemeStore } from "./store/useThemeStore";
-import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
 
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
-//import ProtocolBootLoader from "./components/BootLoader";
-import { audioManager } from "./lib/audioManager";
+//mport SettingsPage from "./pages/SettingsPage";
+//mport ProfilePage from "./pages/ProfilePage";
 
+import { Routes, Route, Navigate } from "react-router-dom";
+import useAuthStore from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
+import ProtocolBootLoader from "./components/BootLoader";
+
+import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
   const { theme } = useThemeStore();
-  
+
   useEffect(() => {
-    audioManager.load("boot", "/auth.mp3", 1);
-    audioManager.load("auth", "/auth.mp3", 1);
-    //audioManager.load("enter", "/sounds/enter.mp3", 1);
-  }, []);
+    checkAuth();
+  }, [checkAuth]);
 
+  console.log({ authUser });
 
-  
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+      <h3 className="text-bold text-red-500">loading yeey</h3>
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
   return (
     <div data-theme={theme}>
+      
+
       <Routes>
-        <Route path="/" element={<HomePage  />}/>
-        <Route path="/signup" element={<SignUpPage /> } />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/chat" />} />
+        <Route path="/" element={<ProtocolBootLoader/>} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/home" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster />
     </div>
   );
 };
-
 export default App;
